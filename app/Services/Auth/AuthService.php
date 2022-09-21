b02b8607e21f8150a13fb83cbf28db586d400d3c
mrinal975 <mrinalmallik1@gmail.com>
2022-09-21 18:10:15 +0600

    segregate code

--- app/Services/Auth/AuthService.php
+++ app/Services/Auth/AuthService.php
@@ -10,7 +10,6 @@ use App\Engine\HttpStatus;
 
 class AuthService
 {
-    private $user;
 
     public function __construct(private User $user)
     {
@@ -22,14 +21,20 @@ class AuthService
         $credentials = $request->only('email', 'password');
         // Other all user login
         if (auth()->attempt($credentials)) {
-            $token = auth()->user()->createToken('web_access_token')->accessToken;
-            $user = Auth::user();
-            $user->access_token = $token;
-            $user = new AuthLoginResource($user);
-            $user['status'] = HttpStatus::OK;
+            $user = $this->getUserData();
             return response()->json(['data' => $user, HttpStatus::STATUS => HttpStatus::OK], HttpStatus::OK);
         } else {
             return response()->json(['error' => 'UnAuthorised', HttpStatus::STATUS => HttpStatus::UNAUTHORIZED], HttpStatus::OK);
         }
+    }
+
+    public function getUserData(): AuthLoginResource
+    {
+        $token = auth()->user()->createToken('web_access_token')->accessToken;
+        $user = Auth::user();
+        $user->access_token = $token;
+        $user = new AuthLoginResource($user);
+        $user['status'] = HttpStatus::OK;
+        return $user;
     }
 }
