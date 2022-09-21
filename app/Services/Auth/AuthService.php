02e197922a4c53a05eb820fce5891c2be7b96a2e
mrinal975 <mrinalmallik1@gmail.com>
2022-09-21 17:51:53 +0600

    login credential check

--- app/Services/Auth/AuthService.php
+++ app/Services/Auth/AuthService.php
@@ -5,6 +5,7 @@ namespace App\Services\Auth;
 use App\Models\User;
 use App\Http\Requests\Login\LoginRequest;
 use Illuminate\Support\Facades\Auth;
+use App\Engine\HttpStatus;
 
 class AuthService
 {
@@ -13,18 +14,19 @@ class AuthService
         //
     }
 
-    public function loginUser(LoginRequest $request): User
+    public function loginUser(LoginRequest $request)
     {
-        $user = $this->userService->getByEmail($request->email);
-
         $credentials = $request->only('email', 'password');
-
         // Other all user login
         if (auth()->attempt($credentials)) {
             $token = auth()->user()->createToken('web_access_token')->accessToken;
             $user = Auth::user();
             $user->access_token = $token;
+            // $user = new AuthLoginResource($user);
+            $user['status'] = HttpStatus::OK;
+            return response()->json(['data' => $user, HttpStatus::STATUS => HttpStatus::OK], HttpStatus::OK);
         } else {
+            return response()->json(['error' => 'UnAuthorised', HttpStatus::STATUS => HttpStatus::UNAUTHORIZED], HttpStatus::OK);
         }
     }
 }
