27f60e5c8f50e2819ad22372b5f652415fe83739
mrinal975 <mrinalmallik1@gmail.com>
2022-09-21 18:06:50 +0600

    get data basis on resource

--- app/Http/Resources/Auth/AuthLoginResource.php
+++ app/Http/Resources/Auth/AuthLoginResource.php
@@ -14,6 +14,12 @@ class AuthLoginResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'role_id' => $this->role_id,
+            'access_token' => $this->access_token,
+        ];
     }
 }
--- app/Services/Auth/AuthService.php
+++ app/Services/Auth/AuthService.php
@@ -5,6 +5,7 @@ namespace App\Services\Auth;
 use App\Models\User;
 use App\Http\Requests\Login\LoginRequest;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\Auth\AuthLoginResource;
 use App\Engine\HttpStatus;
 
 class AuthService
@@ -24,7 +25,7 @@ class AuthService
             $token = auth()->user()->createToken('web_access_token')->accessToken;
             $user = Auth::user();
             $user->access_token = $token;
-            // $user = new AuthLoginResource($user);
+            $user = new AuthLoginResource($user);
             $user['status'] = HttpStatus::OK;
             return response()->json(['data' => $user, HttpStatus::STATUS => HttpStatus::OK], HttpStatus::OK);
         } else {
