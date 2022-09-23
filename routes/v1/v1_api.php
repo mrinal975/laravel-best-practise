3c854c0e71e57027bdf6bda1c42084120c019ae8
mrinal975 <mrinalmallik1@gmail.com>
2022-09-23 22:18:53 +0600

    menu button permission

--- routes/v1/v1_api.php
+++ routes/v1/v1_api.php
@@ -4,6 +4,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Gender\GenderController;
+use App\Http\Controllers\Role\RoleController;
+use App\Http\Controllers\RolePermission\RolePermissionController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -11,4 +13,15 @@ Route::group(['middleware' => ['auth:api']], function () {
 
     Route::resource('/gender', GenderController::class);
 
+
+    //page permission
+    Route::resource('roles', RoleController::class);
+    Route::resource('role-permission', RolePermissionController::class);
+    Route::get('role-permission-page/{roleId}', [RolePermissionController::class, 'groupWisePage']);
+    Route::get('/pages-with-permission', [RolePermissionController::class, 'getPages']);
+    Route::get('/pages-show', [RolePermissionController::class, 'showPages']);
+    Route::get('/check-page-permission', [RolePermissionController::class, 'checkPagePermission']);
+    Route::resource('/role-assign', RoleAssign\RoleAssignController::class);
+    Route::get('permission/button', [RolePermissionController::class, 'getButtonPermission']);
+    // end page permission
 });
