ce51fdc765d806cc4620ac0bbc770748b78b1845
mrinal975 <mrinalmallik1@gmail.com>
2022-09-23 22:26:08 +0600

    role assign base model implement

--- app/Models/RoleAssign/RoleAssign.php
+++ app/Models/RoleAssign/RoleAssign.php
@@ -2,10 +2,42 @@
 
 namespace App\Models\RoleAssign;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Base\BaseModel;
+use App\Models\Role\Role;
 
-class RoleAssign extends Model
+class RoleAssign extends BaseModel
 {
-    use HasFactory;
+    protected $table="users";
+
+    public function __construct()
+    {
+        parent::__construct($this);
+    }
+
+    public function serializerFields()
+    {
+        return ['id', 'name','role_id', 'role__name', 'created_by', 'updated_by'];
+    }
+
+    static public function postserializerFields()
+    {
+        return ['role_id', 'is_active', 'created_by', 'updated_by'];
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class, 'role_id');
+    }
+
+    static public function fieldsValidator()
+    {
+        return [
+            'role_id' => 'required',
+        ];
+    }
+
+    public function exportTableFields() {
+        return['id', 'name'];
+    }
+}
 }
