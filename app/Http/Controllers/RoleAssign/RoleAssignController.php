9c525c7cddabd10616b26874b7c789171abd5edd
mrinal975 <mrinalmallik1@gmail.com>
2022-09-23 22:25:24 +0600

    base controller added

--- app/Http/Controllers/RoleAssign/RoleAssignController.php
+++ app/Http/Controllers/RoleAssign/RoleAssignController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\RoleAssign;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Base\BaseController;
+use App\Models\RoleAssign\RoleAssign;
 
-class RoleAssignController extends Controller
+class RoleAssignController extends BaseController
 {
-    //
+    function __construct(RoleAssign $roleAssign)
+    {
+        $this->entityInstance = $roleAssign;
+        parent::__construct();
+    }
+
 }
---
+++ app/Models/RoleAssign/RoleAssign.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\RoleAssign;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RoleAssign extends Model
+{
+    use HasFactory;
+}
