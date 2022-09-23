80cb66d106b3e876dd40bdb241a3396f2d0bbe4a
mrinal975 <mrinalmallik1@gmail.com>
2022-09-23 21:45:41 +0600

    base controller added

--- app/Http/Controllers/Role/RoleController.php
+++ app/Http/Controllers/Role/RoleController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\Role;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Base\BaseController;
 use Illuminate\Http\Request;
+use App\Models\Role\Role;
 
-class RoleController extends Controller
+class RoleController extends BaseController
 {
-    //
+    function __construct(Role $role)
+    {
+        $this->entityInstance = $role;
+        parent::__construct();
+    }
 }
