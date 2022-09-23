eca6a40ac75a07fbedaa4aceebfcaf84b7d92bd2
mrinal975 <mrinalmallik1@gmail.com>
2022-09-23 21:31:21 +0600

    Role role permission and button permission model migration created

---
+++ app/Models/ButtonPermission/ButtonPermission.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\ButtonPermission;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ButtonPermission extends Model
+{
+    use HasFactory;
+}
