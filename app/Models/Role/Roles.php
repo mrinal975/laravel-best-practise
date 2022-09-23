a66940df904c5453c4d230fe47ba1ea3ad7d33bd
mrinal975 <mrinalmallik1@gmail.com>
2022-09-23 21:46:37 +0600

    base model and implementation

--- app/Models/Role/Roles.php
+++ app/Models/Role/Roles.php
@@ -3,9 +3,48 @@
 namespace App\Models\Role;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Base\BaseModel;
+use App\Manager\PermissionManager\PermissionManager;
 
-class Roles extends Model
+class Role extends BaseModel
 {
     use HasFactory;
+    public function __construct()
+    {
+        parent::__construct($this);
+    }
+
+    public function getPermissionAttribute()
+    {
+        return ( new PermissionManager())->getPageMenuMinTree($this->id);
+    }
+
+    public function serializerFields()
+    {
+        return ['id', 'name', 'permission', 'is_active', 'created_by', 'updated_by'];
+    }
+
+    public static function postserializerFields()
+    {
+        return ['name', 'is_active', 'created_by', 'updated_by'];
+    }
+
+    /**
+     * @param NULL
+     * @return Array
+     */
+    public static function fieldsValidator()
+    {
+        return [
+            'name' => 'required',
+        ];
+    }
+    /**
+     * @param NULL
+     * @return Array
+     */
+    public function exportTableFields()
+    {
+        return['id', 'name'];
+    }
 }
